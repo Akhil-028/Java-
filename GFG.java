150e3d26ebe8fe44bd675d93174fb84d5438f9c4
Akhil-028 <nameisakhil2@gmail.com>
2024-07-22 14:39:10 +0530

    Add files via upload

---
+++ GFG.java
@@ -0,0 +1,17 @@
+import java.io.*; 
+import java.util.*; 
+
+class GFG { 
+	 
+	public static void main(String[] args) 
+	{ 
+	
+        char c = 'G'; 
+
+		
+        String s = Character.toString(c); 
+
+	System.out.println( "Char to String using Character.toString():"+ " " + s); 
+
+	} 
+}
